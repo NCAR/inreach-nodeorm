14dba0867dc4eae6b249776111192dde5d62cc98
John J. Allison <jja@ucar.edu>
2015-05-18 16:31:49 -0600

    mv routes to /inreach

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 var saver = require('../saver');
 
+router.all('/', function (req, res, next) {
+  res.sendStatus(404);
+});
+
 /* GET home page. */
-router.get('/', saver.get);
+router.get('/inreach', saver.get);
 
 /* POST home page. */
-router.post('/', saver.save);
+router.post('/inreach', saver.save);
 
 module.exports = router;
